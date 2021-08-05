0d1287d5bacea756e0074c9bf023043104b554d4
AdiBoiSquad <adiboisquad@gmail.com>
2021-08-05 13:56:33 +0100

    working clien and server

---
+++ Assets/Scripts/joinlobby.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+using TMPro;
+
+
+public class joinlobby : MonoBehaviour
+{
+    [SerializeField] private NetworkManager networkManager = null;
+
+    public TMP_InputField ipAddressInputField;
+    public GameObject panel;
+
+    public void JoinLobby()
+    {
+        string ipAddress = ipAddressInputField.text;
+
+        networkManager.networkAddress = ipAddress;
+        networkManager.StartClient();
+
+        panel.SetActive(false);
+    }
+}
